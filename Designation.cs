e2faece8dbdd06092a0e4c23042a6e127c4fa703
RAKON\rweerasinghe <ruwan.weerasinghe@rakon.com>
2024-03-28 17:58:39 +0000

    Add by Ruwan

--- Designation.cs
+++ Designation.cs
@@ -4,6 +4,7 @@ public class Designation
 {
     public static void PrintDesignation(string name)
     {
+        //This is a static method . No need of define the instance of the object. 
         Console.WriteLine("Designation is : "+ name);
     }
 } 
