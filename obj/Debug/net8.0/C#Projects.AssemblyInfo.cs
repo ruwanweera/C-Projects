db4bec2f000e1c8262be7b38eb826d0ba05ac947
Ruwan <ruwanweera2000@gmail.com>
2024-03-28 19:52:29 +0000

    Commit by ruwan home pc

--- obj/Debug/net8.0/C#Projects.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#Projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d6fd8b3d09d5f4b0942331a78a46019624c854")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
